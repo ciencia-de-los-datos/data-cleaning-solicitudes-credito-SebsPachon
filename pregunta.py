6fa6db0e6e0679363a44ea8bc914c386f60dcdd3
Sebastian Pachon <scpachong@unal.edu.co>
2022-10-27 21:15:14 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -14,44 +14,40 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
+from lib2to3.pgen2.pgen import DFAState
 import pandas as pd
 import numpy as np
 import re
 
 
 
-
 def clean_data():
     # Inserte su código aquí
     df = pd.read_csv("solicitudes_credito.csv", sep=";",usecols=[1,2,3,4,5,6,7,8,9,])
-
-    ##clean nan
-    # df[df.isna().any(axis=1)]x
-    df.dropna(inplace=True)
+    #clean nan and duplicates
     df.drop_duplicates(inplace=True)
-    #sort columns
-    columns_sort=['sexo', 'tipo_de_emprendimiento', 'idea_negocio','línea_credito', 'barrio', 'monto_del_credito','comuna_ciudadano','estrato','fecha_de_beneficio',]
-    df[columns_sort]
-    #Categoric Variable
+    df.dropna(inplace=True)
+    columns_sort=['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'monto_del_credito',
+        'línea_credito' 'barrio', 'estrato',
+        'comuna_ciudadano', 'fecha_de_beneficio',]
 
-    # df[i]=df[i].apply(lambda x :str(x).lower().strip())
-    # df[i]=df[i].apply(lambda x :str(x).replace('-','').strip().replace('_','').strip())
-
+    #Lower()
     for i in columns_sort[:-3]:
-        print(i)
         df[i]=df[i].apply(lambda x :str(x).lower().strip())
-        if i in ['idea_negocio','línea_credito', 'barrio']:
-            df[i]=df[i].apply(lambda x :str(x).replace('-','').strip().replace('_','').strip())
-        elif i=="monto_del_credito":
-            df[i] =  df[i].apply(lambda x: str(x).strip("$").strip().replace(".00", "").replace(",", ""))
-        
-    #date
-    df["fecha_de_beneficio"]=pd.to_datetime(df["fecha_de_beneficio"], dayfirst=True)
 
-    #Clean duplicates
+    #Replace column by column
+    df.idea_negocio = df.idea_negocio.apply(lambda x: str(x).replace("-"," ").replace("_"," ").strip())
+    df.línea_credito= df.línea_credito.apply(lambda x: str(x).lower().replace("-", " ").replace("_", " ").strip())
+    df.barrio= df.barrio.apply(lambda x: str(x).lower().replace("-"," ").replace("_"," "))
+    df.monto_del_credito= df.monto_del_credito.apply(lambda x: str(x).strip("$").strip().replace(",", ""))
+    df.monto_del_credito= df.monto_del_credito.astype('float')
+
+    #date
+    df.fecha_de_beneficio=pd.to_datetime(df["fecha_de_beneficio"], dayfirst=True)
+
+    #clean nan and duplicates 
     df.drop_duplicates(inplace=True)
     df.dropna(inplace=True)
-
-    df.sexo.value_counts()  
-
     return df
+
+df=clean_data()
